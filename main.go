bdb7b5e88f3e6e4be3007351d9ad3b83f7b0dad9
Chris Kellum <christopherk379@gmail.com>
2025-09-17 21:07:11 -0400

    stuff happened

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+)
+
+const (
+	twitch_sub_url = "wss://eventsub.wss.twitch.tv/ws?keepalive_timeout_seconds=300"
+)
 
 func main() {
+	websocket.DefaultDialer.Dial(twitch_sub_url, nil)
+
 	fmt.Println("Hello World!")
 }
